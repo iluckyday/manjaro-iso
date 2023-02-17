98fd60fa7f48d8bfd6d1ddd54aa22b2537136d55
iluckyday <luckyday1990@mail.com>
2023-02-18 00:21:25 +0800

    test

---
+++ custom.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sed -x
+
+sed 's/sudo git clone .* iso-profiles/true/' manjaro-iso-action/action.yml
+
+git clone --depth 1 https://gitlab.manjaro.org/profiles-and-settings/iso-profiles
+
+for i in $(< exclude.txt)
+do
+	find iso-profiles -type f | xargs sed -i "/^${i}/d'
+done
+
+cat include.txt >> iso-profiles/manjaro/gnome/Packages-Desktop
+cat profile.conf >> iso-profiles/manjaro/gnome/profile.conf
