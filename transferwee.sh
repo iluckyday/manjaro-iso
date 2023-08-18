1bb244d3ca734ddf3815e2da38e55ab1e4047739
iluckyday <luckyday1990@mail.com>
2023-08-19 04:33:34 +0800

    test

--- transferwee.sh
+++ transferwee.sh
@@ -3,12 +3,16 @@ set -ex
 
 curl -skLo /tmp/transferwee.py https://raw.githubusercontent.com/iamleot/transferwee/master/transferwee.py
 
+python3 -version
+python3.11 -version
+python3 /tmp/transferwee.py upload -v /bin/ls
+
 FILE=$1
 
 split --verbose -d -b 1500M ${FILE} ${FILE}.
 for f in $(ls ${FILE}.0*)
 do
-t_data=$(python3.11 /tmp/transferwee.py upload $f)
+t_data=$(python3.11 /tmp/transferwee.py upload -v $f)
 FILENAME=$(basename $f)
 SIZE="$(du -h $f | awk '{print $1}')"
 data="$FILENAME-$SIZE-${t_data}"
