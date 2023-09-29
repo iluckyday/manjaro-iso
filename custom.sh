d3ac30b06a2420b9d9db3a095a03de1b1ac8aeb6
iluckyday <luckyday1990@mail.com>
2023-09-29 22:07:10 +0800

    test

--- custom.sh
+++ custom.sh
@@ -10,6 +10,23 @@ do
 	find iso-profiles -type f -name "Packages-*" | xargs sed -i "/^${i}/d"
 done
 
+# disable Packages-Mhwd
+echo > iso-profiles/shared/Packages-Mhwd
+
+NOEXTRACT="
+[options]
+NoExtract = usr/share/locale/* usr/share/X11/locale/*/* usr/share/i18n/locales/*
+NoExtract = !*locale*/en*/* !usr/share/*locale*/locale.*
+NoExtract = usr/share/man/* !usr/share/man/man*
+NoExtract = usr/share/vim/vim*/lang/*
+NoExtract = usr/include/*
+NoExtract = *__pycache__*
+NoExtract = usr/share/doc/*
+"
+
+echo "${NOEXTRACT}" > /etc/pacman.conf.custom
+sed -i '/pacman.conf/a\          cat /etc/pacman.conf.custom | sudo tee -a /etc/pacman.conf' manjaro-iso-action/action.yml
+
 cat include.txt >> iso-profiles/manjaro/gnome/Packages-Desktop
 cat profile.conf >> iso-profiles/manjaro/gnome/profile.conf
 echo password="\"${MANJARO_PASSWORD}\"" >> iso-profiles/manjaro/gnome/profile.conf
