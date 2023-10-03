43ebf6b3b2b7fc5790151253f62421175324e9a5
iluckyday <luckyday1990@mail.com>
2023-10-04 07:30:19 +0800

    test

--- custom.sh
+++ custom.sh
@@ -31,6 +31,7 @@ echo "${NOEXTRACT}" > /etc/pacman.conf.custom
 sed -i '/pacman.conf/a\          cat /etc/pacman.conf.custom | sudo tee -a /etc/pacman.conf' manjaro-iso-action/action.yml
 sed -i '/install_iso/a\          cat /etc/pacman.conf.custom | sudo tee -a /usr/share/manjaro-tools/pacman-*.conf' manjaro-iso-action/action.yml
 # sed -i '/install_iso/a\          sudo sed -i "2i\set -x" /usr/lib/manjaro-tools/util-iso.sh' manjaro-iso-action/action.yml
+sed -i '/install_iso/a\          sudo sed -i "/copytoram_size=/i\\\    copytoram=\\\"y\\\"" /etc/initcpio/hooks/miso' manjaro-iso-action/action.yml
 sed -i '/install_iso/a\          sudo sed -i "/Generating SquashFS/i\\\    sed -i \\\"/configure_language/d\\\" \\\${src}/usr/bin/manjaro-live || true" /usr/lib/manjaro-tools/util-iso.sh' manjaro-iso-action/action.yml
 sed -i '/install_iso/a\          sudo sed -i "/Generating SquashFS/i\\\    rm -rf \\\${src}/usr/lib/modules/*/vmlinuz" /usr/lib/manjaro-tools/util-iso.sh' manjaro-iso-action/action.yml
 sed -i '/install_iso/a\          sudo sed -i "/Generating SquashFS/i\\\    rm -rf \\\${src}/usr/share/i18n/locales/*" /usr/lib/manjaro-tools/util-iso.sh' manjaro-iso-action/action.yml
